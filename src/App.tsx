d38cffbb6b5519ee2f0c4b79ba89e5badb22b4a6
James Johnson <james1q2w3e@gmail.com>
2023-03-09 18:31:14 -0500

    Initial Kermit

---
+++ src/App.tsx
@@ -0,0 +1,112 @@
+import { Fragment, useCallback, useEffect, useMemo, useState } from "react"
+import { InputSelect } from "./components/InputSelect"
+import { Instructions } from "./components/Instructions"
+import { Transactions } from "./components/Transactions"
+import { useEmployees } from "./hooks/useEmployees"
+import { usePaginatedTransactions } from "./hooks/usePaginatedTransactions"
+import { useTransactionsByEmployee } from "./hooks/useTransactionsByEmployee"
+import { EMPTY_EMPLOYEE } from "./utils/constants"
+import { Employee } from "./utils/types"
+
+export function App() {
+  const { data: employees, ...employeeUtils } = useEmployees()
+  const { data: paginatedTransactions, ...paginatedTransactionsUtils } = usePaginatedTransactions()
+  const { data: transactionsByEmployee, ...transactionsByEmployeeUtils } = useTransactionsByEmployee()
+  const [isLoading, setIsLoading] = useState(false)
+
+  const transactions = useMemo(
+    () => paginatedTransactions?.data ?? transactionsByEmployee ?? null,
+    [paginatedTransactions, transactionsByEmployee]
+  )
+
+  // Bug #5
+  const paginateTransactions = useCallback(async () => {
+    await paginatedTransactionsUtils.fetchAll()
+  }, [paginatedTransactionsUtils])
+
+  const loadAllTransactions = useCallback(async () => {
+    setIsLoading(true)
+    transactionsByEmployeeUtils.invalidateData()
+
+    await employeeUtils.fetchAll()
+    // await paginatedTransactionsUtils.fetchAll()
+
+    setIsLoading(false)
+
+    // Bug #5 - call paginate when employees are loaded
+    paginateTransactions()
+  }, [employeeUtils, paginateTransactions, transactionsByEmployeeUtils])
+
+  const loadTransactionsByEmployee = useCallback(
+    async (employeeId: string) => {
+      paginatedTransactionsUtils.invalidateData()
+      await transactionsByEmployeeUtils.fetchById(employeeId)
+    },
+    [paginatedTransactionsUtils, transactionsByEmployeeUtils]
+  )
+
+  useEffect(() => {
+    if (employees === null && !employeeUtils.loading) {
+      loadAllTransactions()
+    }
+  }, [employeeUtils.loading, employees, loadAllTransactions])
+
+  // Bug #6 - assign true/false if there is a next page, also check if it's not filtered by employee
+  const validateShowMoreButton = paginatedTransactions?.nextPage !== null &&
+        transactionsByEmployee === null && transactions !== null;
+
+  return (
+    <Fragment>
+      <main className="MainContainer">
+        <Instructions />
+
+        <hr className="RampBreak--l" />
+
+        <InputSelect<Employee>
+          isLoading={isLoading}
+          defaultValue={EMPTY_EMPLOYEE}
+          items={employees === null ? [] : [EMPTY_EMPLOYEE, ...employees]}
+          label="Filter by employee"
+          loadingLabel="Loading employees"
+          parseItem={(item) => ({
+            value: item.id,
+            label: `${item.firstName} ${item.lastName}`,
+          })}
+          onChange={async (newValue) => {
+            if (newValue === null) {
+              return
+            } else if( // bug #3 - Blank option is filled by EMPTY_EMPLOYEE, so we just check for it
+              newValue.id === EMPTY_EMPLOYEE.id &&
+              newValue.firstName === EMPTY_EMPLOYEE.firstName &&
+              newValue.lastName === EMPTY_EMPLOYEE.lastName
+              ) {
+                await loadAllTransactions();
+                return
+            }
+
+            await loadTransactionsByEmployee(newValue.id)
+          }}
+        />
+
+        <div className="RampBreak--l" />
+
+        <div className="RampGrid">
+          <Transactions transactions={transactions} />
+
+          {/* Bug #6 */}
+          {validateShowMoreButton ? (
+            <button
+              className="RampButton"
+              disabled={paginatedTransactionsUtils.loading}
+              onClick={async () => {
+                await loadAllTransactions()
+              }}
+            >
+              View More
+            </button>
+          ) : null}
+        </div>
+      </main>
+    </Fragment>
+  )
+}
---
+++ src/components/Transactions/index.tsx
@@ -0,0 +1,41 @@
+import { useCallback } from "react"
+import { useCustomFetch } from "src/hooks/useCustomFetch"
+import { SetTransactionApprovalParams } from "src/utils/types"
+import { TransactionPane } from "./TransactionPane"
+import { SetTransactionApprovalFunction, TransactionsComponent } from "./types"
+
+export const Transactions: TransactionsComponent = ({ transactions }) => {
+  const { fetchWithoutCache, loading, clearCacheByEndpoint } = useCustomFetch()
+
+  const setTransactionApproval = useCallback<SetTransactionApprovalFunction>(
+    async ({ transactionId, newValue }) => {
+      await fetchWithoutCache<void, SetTransactionApprovalParams>("setTransactionApproval", {
+        transactionId,
+        value: newValue,
+      })
+
+      // Bug #7 - We already had this written for us in "useCustomFetch.tsx"
+      // We need to reset the cache, so we are passing in the params we need to reset
+      clearCacheByEndpoint(['setTransactionApproval', 'paginatedTransactions', "transactionsByEmployee"])
+
+    },
+    [fetchWithoutCache, clearCacheByEndpoint]
+  )
+
+  if (transactions === null) {
+    return <div className="RampLoading--container">Loading...</div>
+  }
+
+  return (
+    <div data-testid="transaction-container">
+      {transactions.map((transaction) => (
+        <TransactionPane
+          key={transaction.id}
+          transaction={transaction}
+          loading={loading}
+          setTransactionApproval={setTransactionApproval}
+        />
+      ))}
+    </div>
+  )
+}
